ee763e9e5c179f51d3489a0e4a1535eb1d628936
LewsTherinTelescope <92685708+LewsTherinTelescope@users.noreply.github.com>
2022-12-08 15:28:25 -0600

    Slight tweaks to 2022 Day 6
    Generally not going to be messing with things after committing,
    but in case I refer back to it in the future, there's a few tools
    I'd like to remind myself of.

--- src/year2022/day06/Main.kt
+++ src/year2022/day06/Main.kt
@@ -12,11 +12,9 @@ fun main() = runIt(
 	testAnswerPart2 = 19,
 )
 
-fun part1(input: String) = input.findMarkerEndPos(markerLength = 4)
+fun part1(input: String) = input.findMarkerEndPos(markerSize = 4)
 
-fun part2(input: String) = input.findMarkerEndPos(markerLength = 14)
+fun part2(input: String) = input.findMarkerEndPos(markerSize = 14)
 
-fun String.findMarkerEndPos(markerLength: Int) = this
-	.windowed(markerLength) { it.toHashSet().size == it.length }
-	.indexOfFirst { it }
-	.plus(markerLength)
+fun CharSequence.findMarkerEndPos(markerSize: Int) = 
+	windowedSequence(markerSize).indexOfFirst { it.toHashSet().size == it.length } + markerSize
